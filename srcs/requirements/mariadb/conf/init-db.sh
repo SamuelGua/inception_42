9f5c6c633b306c937a2722dff275bbf0f3d9c3b1
SamuelGua <samuel.cely@hotmail.fr>
2024-06-21 21:52:13 +0200

    ___

--- srcs/requirements/mariadb/conf/init-db.sh
+++ srcs/requirements/mariadb/conf/init-db.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+print_green() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+print_green "1 : Starting..."
+
 while ! mysqladmin ping --silent; do
+    print_green "Waiting for MariaDB..."
     sleep 1
 done
 
-#mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
+print_green "2 : MariaDB setup..."
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+print_green "3 : Initialization script completed."
+
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+
+print_green "4 : MariaDB has been stopped."
+
+exec mysqld_safe --datadir=/var/lib/mysql
