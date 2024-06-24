44da0f4e2a84ced6ee4bbb3d3b9c6c98e08de205
scely <scely@scely42.scely.42.fr>
2024-06-24 13:46:08 +0200

    fix mariadb -root -p

--- srcs/requirements/mariadb/conf/init-db.sh
+++ srcs/requirements/mariadb/conf/init-db.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#a effacer lors
+SQL_DATABASE=samuel
+SQL_USER=sam
+SQL_ROOT_PASSWORD=123456
+SQL_PASSWORD=123456
+
 print_green() {
     echo -e "\033[32m$1\033[0m"
 }
@@ -17,7 +23,7 @@ mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
 print_green "3 : Initialization script completed."
 
@@ -25,4 +31,7 @@ mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 
 print_green "4 : MariaDB has been stopped."
 
+print_green "everything it's ok or not"
+exit 1
 exec mysqld_safe --datadir=/var/lib/mysql
+
